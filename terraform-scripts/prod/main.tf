06f801187ee5310e1bd9529e12db0f0075e96c7c
aaronkvirts <aaronk@virtuosity-global.com>
2023-06-16 01:14:57 +0800

    refactor

--- terraform-scripts/prod/main.tf
+++ terraform-scripts/prod/main.tf
@@ -24,12 +24,26 @@ provider "alicloud" {
   region     = "ap-southeast-3"
 }
 
+/*
 module "ecs" {
-  source             = "../modules/ecs"
+  source             = "resource-modules/ecs"
   ecs_rg_id          = "rg-aek4rvnkip7bedq"
   ecs_disk_name      = "Demo Test Disk"
   ecs_instance_name  = "Demo Test ECS Instance"
   ecs_vswitch_id     = "vsw-8psjrm8g2zfrhlkhmy0sm"
   ecs_security_group = "sg-8ps6mar1u7lwnb7kyr5i"
   ecs_disk_size      = 40
+}
+*/
+
+resource "alicloud_instance" "ecs_instance_nano" {
+  security_groups      = var.ecs_security_group
+  instance_type        = "ecs.t5-lc2m1.nano"
+  resource_group_id    = var.ecs_rg_id
+  system_disk_category = "cloud_efficiency"
+  system_disk_name     = var.ecs_disk_name
+  image_id             = "aliyun_3_x64_20G_alibase_20230424.vhd"
+  instance_name        = var.ecs_instance_name
+  vswitch_id           = var.ecs_vswitch_id
+  system_disk_size     = var.ecs_disk_size
 }
---
+++ terraform-scripts/prod/variables.tf
@@ -0,0 +1,35 @@
+variable "ecs_security_group" {
+    type = list(string)
+    default = [ "sg-8ps2di45ns5shx56ro6a" ]
+    description = "Security Group ID"
+}
+
+variable "ecs_disk_name" {
+    type = string
+    default = "Demo Test Disk"
+    description = "ECS Disk Name"
+}
+
+variable "ecs_disk_size" {
+    type = number
+    default = 40
+    description = "ECS Disk Size"
+}
+
+variable "ecs_instance_name" {
+    type = string
+    default = "Demo Test ECS Instance"
+    description = "ECS Instance Name"
+}
+
+variable "ecs_vswitch_id" {
+    type = string
+    default = "vsw-8psjrm8g2zfrhlkhmy0sm"
+    description = "vSwitch ID"
+}
+
+variable "ecs_rg_id" {
+    type = string
+    default = "rg-aek4rvnkip7bedq"
+    description = "Resource Group ID"
+}
