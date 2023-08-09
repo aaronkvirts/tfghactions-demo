ba7427ecd4d7f9c5e95dd29ac53bf1c5012f6b1c
aaronkvirts <aaronk@virtuosity-global.com>
2023-08-09 11:51:31 +0800

    Update outputs.tf

--- resource-groups/prod/outputs.tf
+++ resource-groups/prod/outputs.tf
@@ -3,12 +3,17 @@ output "ecs_instance_1_id" {
   description = "ID for the deployed alicloud ECS instance"
 }
 
-output "ecs_instance_2_id" {
-  value       = alicloud_instance.ecs_instance_2.id
-  description = "ID for the deployed alicloud ECS instance"
+output "redis_dual_instance_1_id" {
+  value       = alicloud_instance.redis_dual_instance_1.id
+  description = "ID for the deployed alicloud Redis instance"
 }
 
-output "ecs_instance_3_id" {
-  value       = alicloud_instance.ecs_instance_3.id
-  description = "ID for the deployed alicloud ECS instance"
+output "rds_ha_instance_1_id" {
+  value       = alicloud_instance.rds_ha_instance_1.id
+  description = "ID for the deployed alicloud RDS instance"
+}
+
+output "ack_pro_cluster_instance_1_id" {
+  value       = alicloud_instance.ack_pro_cluster_instance_1.id
+  description = "ID for the deployed alicloud ACK Pro instance"
 }
