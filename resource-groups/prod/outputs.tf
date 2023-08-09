b6077342a348e25059f3d4669e7384f40a8cffdb
aaronkvirts <aaronk@virtuosity-global.com>
2023-08-09 11:57:00 +0800

    Update outputs.tf

--- resource-groups/prod/outputs.tf
+++ resource-groups/prod/outputs.tf
@@ -4,16 +4,16 @@ output "ecs_instance_1_id" {
 }
 
 output "redis_dual_instance_1_id" {
-  value       = alicloud_instance.redis_dual_instance_1.id
+  value       = alicloud_kvstore_instance.redis_dual_instance_1.id
   description = "ID for the deployed alicloud Redis instance"
 }
 
 output "rds_ha_instance_1_id" {
-  value       = alicloud_instance.rds_ha_instance_1.id
+  value       = alicloud_db_instance.rds_ha_instance_1.id
   description = "ID for the deployed alicloud RDS instance"
 }
 
 output "ack_pro_cluster_instance_1_id" {
-  value       = alicloud_instance.ack_pro_cluster_instance_1.id
+  value       = alicloud_cs_managed_kubernetes.ack_pro_cluster_instance_1.id
   description = "ID for the deployed alicloud ACK Pro instance"
 }
