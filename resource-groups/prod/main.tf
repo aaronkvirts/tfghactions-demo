3d9a34891e2646882eee285a53ca9e0013152671
aaronkvirts <aaronk@virtuosity-global.com>
2023-08-10 16:56:49 +0800

    Update main.tf

--- resource-groups/prod/main.tf
+++ resource-groups/prod/main.tf
@@ -25,7 +25,7 @@ resource "alicloud_instance" "ecs_instance_1" {
   instance_type        = "ecs.t5-lc2m1.nano"
   system_disk_category = "cloud_efficiency"
   system_disk_name     = "ECS-Cloud-Disk-1"
-  image_id             = "aliyun_3_x64_20G_alibase_20230424.vhd"
+  //image_id             = "aliyun_3_x64_20G_alibase_20230424.vhd"
   instance_name        = "ECS-Instance-1"
   vswitch_id           = "vsw-8psjrm8g2zfrhlkhmy0sm"
   system_disk_size     = var.ecs_disk_size
